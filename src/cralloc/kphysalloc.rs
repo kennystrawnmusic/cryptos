bd51cd89d862595cc15d6c9ecf965b07e3bd5ea7
Kenny Strawn <Kenny.Strawn@gmail.com>
2022-08-12 21:48:56 -0700

    Didn't realize `Mapper` has an `unmap()` method

--- src/cralloc/kphysalloc.rs
+++ src/cralloc/kphysalloc.rs
@@ -1,3 +1,5 @@
+use x86_64::structures::paging::mapper::UnmapError;
+
 use {
     crate::{get_phys_offset, map_page},
     core::ptr::{read_volatile, write_volatile},
@@ -38,16 +40,23 @@ pub fn kphysfree(size: usize) {
     // same as before: use a test page to determine proper alignment
     let test_addr = VirtAddr::new(size as u64 + unsafe { get_phys_offset() });
     let test_page = Page::<Size4KiB>::containing_address(test_addr);
-    let mut virt = (test_page.start_address().as_u64() as usize)
+    let mut _virt = (test_page.start_address().as_u64() as usize)
         + crate::page_align(size as u64, test_addr.as_u64());
 
-    for _ in (size / crate::page_align(size as u64, test_addr.as_u64()))..0 {
-        map_page!(
-            size,
-            virt,
-            Size4KiB,
-            PageTableFlags::empty() // clear all flags, including the ones indicating that the page exists
-        );
-        virt -= test_page.size() as usize; // reverse order
+    for p in (size / crate::page_align(size as u64, test_addr.as_u64()))..0 {
+        let test_addr_inner = VirtAddr::new((size as u64 + ((test_page.size() as u64) * (p as u64))) as u64);
+        let to_free = Page::<Size4KiB>::containing_address(test_addr_inner);
+
+        let flush = if let Ok((_, free_flush)) = crate::MAPPER.get().unwrap().lock().unmap(to_free) {
+            Some(free_flush)
+        } else {
+            None
+        };
+
+        if let Some(free_flush) = flush {
+            free_flush.flush();
+        }
+
+        _virt -= test_page.size() as usize; // reverse order
     }
 }
