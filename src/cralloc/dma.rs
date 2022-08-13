4ba2dcb3473a357de0b2561bc76b55bc40e01ed2
Kenny Strawn <Kenny.Strawn@gmail.com>
2022-08-13 15:30:45 -0700

    Final stretch

--- src/cralloc/dma.rs
+++ src/cralloc/dma.rs
@@ -1,7 +1,7 @@
 use super::kphysalloc::{kphysalloc, kphysmap, PageBox};
-use core::mem::MaybeUninit;
+use core::{mem::MaybeUninit, ops::{Deref, DerefMut}};
 
-pub struct Dma<T> {
+pub struct Dma<T: ?Sized> {
     phys: usize,
     size: usize,
     virt: *mut T,
@@ -64,5 +64,77 @@ impl<T> Dma<MaybeUninit<T>> {
             size,
             virt: virt as *mut T,
         }
+    }
+}
+
+impl<T> Dma<[T]> {
+    pub fn from_pagebox_uninit_unsized(
+        pb: PageBox<T>,
+        len: usize,
+    ) -> syscall::Result<Dma<[MaybeUninit<T>]>> {
+        let max = pb.size() / core::mem::size_of::<T>();
+
+        // hate assertions because they always give the wrong information
+        if len > max {
+            panic!("Slice index out of bounds")
+        }
+
+        let phys = pb.physical_address();
+        let size = core::mem::size_of::<T>();
+
+        Ok(Dma {
+            phys,
+            size,
+            virt: unsafe {
+                core::slice::from_raw_parts_mut(kphysmap(phys, size) as *mut MaybeUninit<T>, len)
+            } as *mut [MaybeUninit<T>],
+        })
+    }
+
+    pub fn from_pagebox_zeroed_unsized(
+        pb: PageBox<T>,
+        len: usize,
+    ) -> syscall::Result<Dma<[MaybeUninit<T>]>> {
+        let ns = Self::from_pagebox_uninit_unsized(pb, len)?;
+        unsafe { core::ptr::write_bytes(ns.virt as *mut MaybeUninit<u8>, 0, ns.size) }
+        Ok(ns)
+    }
+
+    pub unsafe fn zeroed_unsized(byte_count: usize) -> syscall::Result<Self> {
+        let pb = PageBox::new(core::mem::size_of::<T>() * byte_count)?;
+        Ok(Self::from_pagebox_zeroed_unsized(pb, byte_count)?.assume_init())
+    }
+}
+
+impl<T> Dma<[MaybeUninit<T>]> {
+    pub unsafe fn assume_init(self) -> Dma<[T]> {
+        let &Dma { phys, size, virt } = &self;
+        core::mem::forget(self);
+
+        Dma {
+            phys,
+            size,
+            virt: virt as *mut [T],
+        }
+    }
+}
+
+impl<T: ?Sized> Deref for Dma<T> {
+    type Target = T;
+    fn deref(&self) -> &T {
+        unsafe {&*self.virt}
+    }
+}
+
+impl<T: ?Sized> DerefMut for Dma<T> {
+    fn deref_mut(&mut self) -> &mut T {
+        unsafe {&mut *self.virt}
+    }
+}
+
+impl<T: ?Sized> Drop for Dma<T> {
+    fn drop(&mut self) {
+        unsafe { core::ptr::drop_in_place(self.virt) };
+        super::kphysalloc::kphysfree(self.virt as *mut u8 as usize, self.size);
     }
 }
