2b5c1ead98e0a35bc5d3d1032c2dcbb3af74d852
Kenny Strawn <Kenny.Strawn@gmail.com>
2022-12-15 17:34:05 -0800

    HMFS: Prepare for future partition creation

--- src/hmfs.rs
+++ src/hmfs.rs
@@ -106,11 +106,11 @@ impl Properties {
     }
 }
 
-// Needed to allow writing HashMaps directly to the disk
-pub fn hashmap_bytes<K, V>(map: HashMap<K, V>) -> &'static mut [u8] {
-    let map_addr = &map as *const _ as usize as u64;
+// Pave the way for (partition) formatting
+pub fn root_entry_bytes(entry: RootEntry) -> &'static mut [u8] {
+    let map_addr = &entry as *const _ as usize as u64;
     unsafe {
-        core::slice::from_raw_parts_mut(map_addr as *mut u8, core::mem::size_of::<HashMap<K, V>>())
+        core::slice::from_raw_parts_mut(map_addr as *mut u8, core::mem::size_of::<RootEntry>())
     }
 }
 
