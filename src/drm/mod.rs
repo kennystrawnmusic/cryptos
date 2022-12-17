e8e4bea60b727455072d5dc0afeabfba73f95421
Kenny Strawn <Kenny.Strawn@gmail.com>
2022-12-17 13:28:12 -0800

    embedded_graphics: Make it easy for the framebuffer to be cloned

--- src/drm/mod.rs
+++ src/drm/mod.rs
@@ -1,8 +1,17 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 
 use alloc::vec::Vec;
-use bootloader_api::config::FrameBuffer;
+use bootloader_api::info::{FrameBuffer, FrameBufferInfo};
 use spin::RwLock;
+
+use crate::FRAMEBUFFER_ADDR;
 
 #[allow(dead_code)] // needed for later
 pub static COMPOSITING_TABLE: RwLock<Vec<FrameBuffer>> = RwLock::new(Vec::new());
+
+// Needed to allow rendering in multiple separate buffers — the very definition of compositing
+pub fn clone_buffer(old: FrameBuffer) -> FrameBuffer {
+    unsafe {
+        FrameBuffer::new(FRAMEBUFFER_ADDR, old.info())
+    }
+}
