95d35e5604e1c0d460fdb502b7baf2e06d60c9c6
Kenny Strawn <Kenny.Strawn@gmail.com>
2022-12-18 16:38:45 -0800

    embedded-graphics: Begin work on CompositingLayer impl

--- src/drm/mod.rs
+++ src/drm/mod.rs
@@ -1,7 +1,8 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 
 use alloc::vec::Vec;
-use bootloader_api::info::{FrameBuffer, FrameBufferInfo};
+use bootloader_api::info::{FrameBuffer, FrameBufferInfo, PixelFormat};
+use embedded_graphics::{prelude::PixelColor, pixelcolor::{Rgb888, Bgr888, Gray8}};
 use core::iter::zip;
 use embedded_graphics_core::geometry::Point;
 use spin::RwLock;
@@ -28,11 +29,44 @@ pub fn render_points(points: impl Iterator<Item = Point>, mut buffer: FrameBuffe
     todo!()
 }
 
+pub enum PixelColorKind {
+    Rgb(Rgb888),
+    Bgr(Bgr888),
+    U8(Gray8),
+}
+
+impl PixelColorKind {
+    pub fn new(info: FrameBufferInfo, red: u8, green: u8, blue: u8) -> Self {
+        let luma = ((red.clone() as u32 * green.clone() as u32 * blue.clone() as u32)/3) as u8;
+        match info.pixel_format {
+            PixelFormat::Rgb => Self::Rgb(Rgb888::new(red.clone(), green.clone(), blue.clone())),
+            PixelFormat::Bgr => Self::Bgr(Bgr888::new(red.clone(), green.clone(), blue.clone())),
+            PixelFormat::U8 => Self::U8(Gray8::new(luma)),
+            _ => panic!("Unknown pixel format")
+        }
+    }
+}
+
 // Idea is to eventually implement DrawTarget for this
 #[allow(dead_code)]
 pub struct CompositingLayer {
+    color: PixelColorKind,
     fb: &'static mut [u8],
     info: FrameBufferInfo,
     x: usize,
     y: usize,
+}
+
+impl CompositingLayer {
+    pub fn new(mut buffer: FrameBuffer, red: u8, green: u8, blue: u8, x: usize, y: usize) -> Self {
+        let info = buffer.info().clone();
+        Self { 
+            color: PixelColorKind::new(info, red, green, blue),
+            // borrow checker throws a fit if I don't do this
+            fb: buffer.buffer_mut().iter().map(|i| i.clone()).collect::<Vec<_>>().leak(),
+            info,
+            x,
+            y
+        }
+    }
 }
