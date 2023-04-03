d6a6d94a7ef4ba1c385e4d97383d7a65134688ef
Kenny Strawn <Kenny.Strawn@gmail.com>
2023-04-03 09:13:05 -0700

    Timer: also use relaxed ordering

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -107,8 +107,8 @@ pub enum IrqIndex {
 }
 
 extern "x86-interrupt" fn timer(_frame: InterruptStackFrame) {
-    TICK_COUNT.fetch_add(1, Ordering::SeqCst);
-    debug!("{:#?}", &TICK_COUNT.load(Ordering::SeqCst));
+    TICK_COUNT.fetch_add(1, Ordering::Relaxed);
+    debug!("{:#?}", &TICK_COUNT.load(Ordering::Relaxed));
     unsafe { LOCAL_APIC.lock().as_mut().unwrap().end_of_interrupt() }
 }
 
