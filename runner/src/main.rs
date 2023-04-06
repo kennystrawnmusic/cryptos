4f1ac1d2b8c23c0f373dcdda27b1c88e843cf78f
Kenny Strawn <Kenny.Strawn@gmail.com>
2023-04-06 15:00:30 -0700

    Don't need to increase VM size anymore

--- runner/src/main.rs
+++ runner/src/main.rs
@@ -11,9 +11,7 @@ fn main() {
     let kdir = Path::new(env!("CARGO_MANIFEST_DIR")).parent().unwrap();
     let out_path = kdir.join("cryptos.img");
 
-    let mut fb = FrameBuffer::default();
-    fb.minimum_framebuffer_height = Some(1920);
-    fb.minimum_framebuffer_width = Some(1080);
+    let fb = FrameBuffer::default();
 
     let mut c = BootConfig::default();
     c.frame_buffer = fb;
