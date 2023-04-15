21dd2f8f9c0fa30f7f1131bcbbd962b31c99b9e5
Kenny Strawn <Kenny.Strawn@gmail.com>
2023-04-15 16:32:26 -0700

    ACPI: More fine-tuning

--- src/acpi_impl.rs
+++ src/acpi_impl.rs
@@ -12,7 +12,7 @@ use raw_cpuid::CpuId;
 use rsdp::Rsdp;
 use x86_64::{instructions::port::Port, structures::paging::FrameAllocator};
 
-use crate::interrupts::{INTA_IRQ, INTB_IRQ, INTC_IRQ, INTD_IRQ};
+use crate::{interrupts::{INTA_IRQ, INTB_IRQ, INTC_IRQ, INTD_IRQ}, OEM_ID};
 
 use {
     crate::{get_phys_offset, map_page},
@@ -547,17 +547,24 @@ pub fn aml_init(tables: &mut AcpiTables<KernelAcpi>) {
     );
 
     let raw_table = unsafe {
-        core::slice::from_raw_parts_mut(
-            aml_virt as *mut u8,
-            dsdt_len.clone() + core::mem::size_of::<SdtHeader>(),
-        )
+        if OEM_ID.get().unwrap().contains("ALASKA") { // BioStar
+            core::slice::from_raw_parts_mut(
+                aml_virt as *mut u8,
+                dsdt_len.clone(),
+            )
+        } else {
+            core::slice::from_raw_parts_mut(
+                aml_virt as *mut u8,
+                dsdt_len.clone() + core::mem::size_of::<SdtHeader>(),
+            )
+        }
     };
 
     if let Ok(()) = aml_ctx.initialize_objects() {
-        let aml_stream = if let Some(_) = CpuId::new().get_hypervisor_info() {
-            raw_table.split_at_mut(core::mem::size_of::<SdtHeader>()).1
+        let aml_stream = if OEM_ID.get().unwrap().contains("ALASKA") { // BioStar
+            raw_table
         } else {
-            raw_table.split_at_mut(core::mem::size_of::<SdtHeader>() - 1).1
+            raw_table.split_at_mut(core::mem::size_of::<SdtHeader>()).1
         };
 
         if let Ok(()) =
