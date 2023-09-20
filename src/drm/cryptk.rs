f803c6708b3e3dcc085ea58a01e5e61a37951ca2
Kenny Strawn <Kenny.Strawn@gmail.com>
2023-09-20 14:01:49 -0700

    CrypTK: Add some more custom types

--- src/drm/cryptk.rs
+++ src/drm/cryptk.rs
@@ -4,30 +4,80 @@ use alloc::vec::Vec;
 
 use super::{Canvas, PixelColorKind};
 
-type Text<'a> = embedded_graphics::text::Text<'a, U8g2TextStyle<PixelColorKind>>;
+pub type Text<'a> = embedded_graphics::text::Text<'a, U8g2TextStyle<PixelColorKind>>;
+
+#[allow(unused)] // not finished
+pub struct WindowControls {
+    background: Rectangle,
+    buttons: [Circle; 3],
+}
 
 #[allow(unused)] // not finished
 pub struct WindowDecoration<'a> {
     background: RoundedRectangle,
     text: Text<'a>,
+    controls: WindowControls,
+}
+
+#[allow(unused)] // not finished
+pub struct Window<'a> {
+    background: RoundedRectangle,
+    decoration: WindowDecoration<'a>,
+}
+
+#[allow(unused)] // not finished
+pub struct Dock {
+    start: RoundedRectangle,
+    mid: Vec<Rectangle>,
+    end: RoundedRectangle,
+}
+
+#[allow(unused)] // not finished
+pub struct TextInput<'a> {
+    background: Rectangle,
+    text: &'a mut Text<'a>, // TODO: replace this with owned type in impls
+}
+
+#[allow(unused)] // not finished
+pub struct TextView<'a> {
+    background: Rectangle,
+    text: Text<'a>,
+}
+
+#[allow(unused)] // not finished
+pub struct Label<'a> {
+    background: RoundedRectangle,
+    text: Text<'a>,
+}
+
+#[allow(unused)] // not finished
+pub struct Search<'a> {
+    background: RoundedRectangle,
+    text: &'a mut Text<'a>, // TODO: replace this with owned type in impls
+}
+
+#[allow(unused)] // not finished
+pub struct Menu<'a> {
+    top: RoundedRectangle,
+    selection: Option<RoundedRectangle>,
+    items: Vec<TextView<'a>>,
+    bottom: RoundedRectangle,
 }
 
 #[non_exhaustive] // not finished
 pub enum WidgetKind<'a> {
     Separator(Line),
-    Window(WindowDecoration<'a>),
-    MinMaxClose((Rectangle, [Circle; 3])),
-    TextInput(RoundedRectangle),
-    TextView(RoundedRectangle),
-    Label(Rectangle),
-    Search(RoundedRectangle),
+    Window(Window<'a>),
+    TextInput(TextInput<'a>),
+    TextView(TextView<'a>),
+    Label(Label<'a>),
+    Search(Search<'a>),
     Tab(Rectangle),
     Scrollbar(Rectangle),
-    Menu(RoundedRectangle),
-    MenuItem(RoundedRectangle),
+    Menu(Menu<'a>),
     Ribbon(Rectangle),
     SystemTray(Rectangle),
-    Dock((RoundedRectangle, Vec<Rectangle>, RoundedRectangle)),
+    Dock(Dock),
     Overview(Rectangle),
     ListView(Rectangle),
     NavView([Rectangle; 2]),
