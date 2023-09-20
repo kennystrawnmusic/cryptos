f1503032e3f4924d30a26a860d8e8a823a1ee509
Kenny Strawn <Kenny.Strawn@gmail.com>
2023-09-20 13:00:46 -0700

    Begin templating the widget toolkit

--- src/drm/cryptk.rs
+++ src/drm/cryptk.rs
@@ -1,1 +1,31 @@
-// TODO: widget toolkit
+use embedded_graphics::primitives::{Rectangle, RoundedRectangle, Circle};
+
+use super::CompositingLayer;
+
+#[non_exhaustive] // not finished
+pub enum WidgetClass {
+    Window(RoundedRectangle),
+    MinMaxClose([Circle; 3]),
+    TextInput(RoundedRectangle),
+    TextView(RoundedRectangle),
+    Search(RoundedRectangle),
+    Tab(Rectangle),
+    Scrollbar(Rectangle),
+    Menu(RoundedRectangle),
+    MenuItem(RoundedRectangle),
+    Ribbon(Rectangle),
+    SystemTray(Rectangle),
+    Dock(RoundedRectangle),
+    Overview(Rectangle),
+    ListView(Rectangle),
+    NavView([Rectangle; 2]),
+    Slider((RoundedRectangle, Circle)),
+    Toggle((RoundedRectangle, Circle)),
+    ProgressBar([RoundedRectangle; 2]),
+}
+
+#[allow(unused)] // not finished
+pub struct Widget {
+    layer: CompositingLayer,
+    class: WidgetClass,
+}
