d329b881dfb9125d00109b918e2e8be5fcd05285
Kenny Strawn <Kenny.Strawn@gmail.com>
2023-09-21 16:33:49 -0700

    rustfmt

--- src/process/signal.rs
+++ src/process/signal.rs
@@ -1,37 +1,88 @@
 use bitflags::bitflags;
 
-bitflags! {
-    pub struct Signal: u8 {
-        const SIGHUP = 1;
-        const SIGINT = 2;
-        const SIGQUIT = 3;
-        const SIGILL = 4;
-        const SIGTRAP = 5;
-        const SIGABRT = 6;
-        const SIGEMT = 7;
-        const SIGFPE = 8;
-        const SIGKILL = 9;
-        const SIGBUS = 10;
-        const SIGSEGV = 11;
-        const SIGSYS = 12;
-        const SIGPIPE = 13;
-        const SIGALRM = 14;
-        const SIGTERM = 15;
-        const SIGURG = 16;
-        const SIGSTOP = 17;
-        const SIGTSTP = 18;
-        const SIGCONT = 19;
-        const SIGCHLD = 20;
-        const SIGTTIN = 21;
-        const SIGTTOU = 22;
-        const SIGIO = 23;
-        const SIGXCPU = 24;
-        const SIGXFSZ = 25;
-        const SIGVTALRM = 26;
-        const SIGPROF = 27;
-        const SIGWINCH = 28;
-        const SIGINFO = 29;
-        const SIGUSR1 = 30;
-        const SIGUSR2 = 31;
+// reuse all the call numbers defined in the redox_syscall crate
+pub use syscall::{
+    SIGABRT, SIGALRM, SIGBUS, SIGCHLD, SIGCONT, SIGFPE, SIGHUP, SIGILL, SIGINT, SIGIO, SIGKILL,
+    SIGPIPE, SIGPROF, SIGPWR, SIGQUIT, SIGSEGV, SIGSTKFLT, SIGSTOP, SIGSYS, SIGTERM, SIGTRAP,
+    SIGTSTP, SIGTTIN, SIGTTOU, SIGURG, SIGUSR1, SIGUSR2, SIGVTALRM, SIGWINCH, SIGXCPU, SIGXFSZ,
+};
+
+// not defined upstream, so adding here
+pub const SIGINFO: usize = 32;
+
+#[derive(Clone, Debug)]
+pub enum Signal {
+    Unknown,
+    SIGHUP,
+    SIGINT,
+    SIGQUIT,
+    SIGILL,
+    SIGTRAP,
+    SIGABRT,
+    SIGBUS,
+    SIGFPE,
+    SIGKILL,
+    SIGUSR1,
+    SIGSEGV,
+    SIGUSR2,
+    SIGPIPE,
+    SIGALRM,
+    SIGTERM,
+    SIGSTKFLT,
+    SIGCHLD,
+    SIGCONT,
+    SIGSTOP,
+    SIGTSTP,
+    SIGTTIN,
+    SIGTTOU,
+    SIGURG,
+    SIGXCPU,
+    SIGXFSZ,
+    SIGVTALRM,
+    SIGPROF,
+    SIGWINCH,
+    SIGIO,
+    SIGPWR,
+    SIGSYS,
+    SIGINFO,
+}
+
+impl Signal {
+    pub fn new(num: usize) -> Self {
+        match num {
+            SIGHUP => Self::SIGHUP,
+            SIGINT => Self::SIGINT,
+            SIGQUIT => Self::SIGQUIT,
+            SIGILL => Self::SIGILL,
+            SIGTRAP => Self::SIGTRAP,
+            SIGABRT => Self::SIGABRT,
+            SIGBUS => Self::SIGBUS,
+            SIGFPE => Self::SIGFPE,
+            SIGKILL => Self::SIGKILL,
+            SIGUSR1 => Self::SIGUSR1,
+            SIGSEGV => Self::SIGSEGV,
+            SIGUSR2 => Self::SIGUSR2,
+            SIGPIPE => Self::SIGPIPE,
+            SIGALRM => Self::SIGALRM,
+            SIGTERM => Self::SIGTERM,
+            SIGSTKFLT => Self::SIGSTKFLT,
+            SIGCHLD => Self::SIGCHLD,
+            SIGCONT => Self::SIGCONT,
+            SIGSTOP => Self::SIGSTOP,
+            SIGTSTP => Self::SIGTSTP,
+            SIGTTIN => Self::SIGTTIN,
+            SIGTTOU => Self::SIGTTOU,
+            SIGURG => Self::SIGURG,
+            SIGXCPU => Self::SIGXCPU,
+            SIGXFSZ => Self::SIGXFSZ,
+            SIGVTALRM => Self::SIGVTALRM,
+            SIGPROF => Self::SIGPROF,
+            SIGWINCH => Self::SIGWINCH,
+            SIGIO => Self::SIGIO,
+            SIGPWR => Self::SIGPWR,
+            SIGSYS => Self::SIGSYS,
+            SIGINFO => Self::SIGINFO,
+            _ => Self::Unknown,
+        }
     }
 }
