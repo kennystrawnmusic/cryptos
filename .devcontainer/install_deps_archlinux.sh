dd4d18431d8f76c99758fa0feedd3595ada8b922
Kenny Strawn <Kenny.Strawn@gmail.com>
2023-10-02 17:23:06 -0700

    Oops

--- .devcontainer/install_deps_archlinux.sh
+++ .devcontainer/install_deps_archlinux.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-pacman -Syu
-pacman -S base base-devel plasma kde-applications sudo qemu-system-x86 edk2-ovmf p7zip
+pacman --noconfirm -Syu
+pacman --noconfirm -S base-devel plasma kde-applications sudo qemu-system-x86 edk2-ovmf p7zip
