6b9441d5d61bae6c5208847123d4e8299ceb704d
Kenny Strawn <Kenny.Strawn@gmail.com>
2023-10-02 17:51:00 -0700

    Oops

--- .devcontainer/install_rust_ubuntu.sh
+++ .devcontainer/install_rust_ubuntu.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 source /etc/profile
-apt-get update && apt-get -y install sudo
 sudo -u vscode curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | bash -s -- --default-toolchain nightly -y
