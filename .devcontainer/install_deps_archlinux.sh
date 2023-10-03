2cedd873957cbf13e0713c3156dfd0c406a361af
Kenny Strawn <Kenny.Strawn@gmail.com>
2023-10-02 17:39:30 -0700

    Oops

--- .devcontainer/install_deps_archlinux.sh
+++ .devcontainer/install_deps_archlinux.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-pacman --needed --noconfirm -Syu base-devel plasma kde-applications sudo qemu-system-x86 edk2-ovmf p7zip
+pacman --needed --noconfirm -Syu base-devel plasma kde-applications qemu-system-x86 edk2-ovmf p7zip
--- .devcontainer/install_rust.sh
+++ .devcontainer/install_rust.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 source /etc/profile
+pacman --noconfirm -Syu sudo
 sudo -u vscode curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | bash -s -- --default-toolchain nightly -y
