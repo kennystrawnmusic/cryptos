5d95a8c2bb9f5f6078650e9eb915bcb2b2a95949
Kenny Strawn <Kenny.Strawn@gmail.com>
2023-10-03 08:43:28 -0700

    rustfmt

--- src/drivers/xhci/mod.rs
+++ src/drivers/xhci/mod.rs
@@ -1,10 +1,12 @@
+use crate::{cralloc::frames::XhciMapper, pci_impl::DeviceKind, FRAME_ALLOCATOR};
 use alloc::boxed::Box;
-use pcics::{Header, header::HeaderType};
-use xhci::{Registers, registers::Operational};
-use crate::{FRAME_ALLOCATOR, cralloc::frames::XhciMapper};
+use pcics::{header::HeaderType, Header};
+use xhci::{registers::Operational, Registers};
 
 pub fn xhci_init(header: &Header, mapper: XhciMapper) -> Option<Operational<XhciMapper>> {
-    if header.class_code.base == 0x0c && header.class_code.sub == 0x03 && header.class_code.interface == 0x30 {
+    if let DeviceKind::UsbController =
+        DeviceKind::new(header.class_code.base as u32, header.class_code.sub as u32)
+    {
         if let HeaderType::Normal(header) = header.header_type.clone() {
             let bar0 = header.base_addresses.orig()[0];
             let bar1 = header.base_addresses.orig()[1];
@@ -12,9 +14,9 @@ pub fn xhci_init(header: &Header, mapper: XhciMapper) -> Option<Operational<Xhci
             let full_bar = bar0 as u64 | ((bar1 as u64) << 32);
 
             let regs = unsafe { Registers::new(full_bar as usize, mapper) };
-            
+
             Some(regs.operational)
-        }  else {
+        } else {
             None
         }
     } else {
