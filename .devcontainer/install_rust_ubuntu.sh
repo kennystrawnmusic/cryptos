a33505879b0d3e4c18904ed72545d143afda1642
Kenny Strawn <Kenny.Strawn@gmail.com>
2023-10-03 20:51:34 +0000

    Finally fixed the Dockerfile scripts

--- .devcontainer/install_rust_ubuntu.sh
+++ .devcontainer/install_rust_ubuntu.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 source /etc/profile
-sudo -u vscode curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | bash -s -- --default-toolchain nightly -y
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sudo -u vscode bash -s -- --default-toolchain nightly -y
