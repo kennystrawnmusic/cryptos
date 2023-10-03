e9e596f0161557fbdb509f67bfb1286dada56ef1
Kenny Strawn <Kenny.Strawn@gmail.com>
2023-10-02 18:06:58 -0700

    Temporary workaround part 2

--- .devcontainer/install_deps_ubuntu.sh
+++ .devcontainer/install_deps_ubuntu.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-apt-get update && apt-get -y install sudo ubuntu-desktop build-essential qemu-system-x86 ovmf p7zip
+apt-get update && apt-get -y install git sudo ubuntu-desktop build-essential qemu-system-x86 ovmf p7zip
