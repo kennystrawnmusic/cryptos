3a16ceab7e815ddb71b1ecdd665936f604475732
Kenny Strawn <Kenny.Strawn@gmail.com>
2023-10-03 09:40:11 -0700

    Begin FOSSPCIDeviceHandle impl

--- src/drivers/xhci/mod.rs
+++ src/drivers/xhci/mod.rs
@@ -1,27 +1,53 @@
-use crate::{cralloc::frames::XhciMapper, pci_impl::DeviceKind, FRAME_ALLOCATOR};
+use crate::{cralloc::frames::XhciMapper, pci_impl::{DeviceKind, FOSSPciDeviceHandle}, FRAME_ALLOCATOR};
 use pcics::{header::HeaderType, Header};
 use spin::RwLock;
 use xhci::Registers;
 
+#[allow(dead_code)] // not finished
 pub(crate) static MAPPER: RwLock<XhciMapper> = RwLock::new(XhciMapper);
 
-pub fn xhci_init(header: &Header) -> Option<Registers<XhciMapper>> {
-    if let DeviceKind::UsbController =
-        DeviceKind::new(header.class_code.base as u32, header.class_code.sub as u32)
-    {
-        if let HeaderType::Normal(header) = header.header_type.clone() {
-            let bar0 = header.base_addresses.orig()[0];
-            let bar1 = header.base_addresses.orig()[1];
+#[allow(dead_code)] // not finished
+pub(crate) struct XhciImpl {
+    pub(crate) regs: Option<Registers<XhciMapper>>,
+}
 
-            let full_bar = bar0 as u64 | ((bar1 as u64) << 32);
+#[allow(dead_code)] // not finished
+impl XhciImpl {
+    pub(crate) fn new(header: &Header) -> Self {
+        Self {
+            regs: {
+                if let DeviceKind::UsbController =
+                    DeviceKind::new(header.class_code.base as u32, header.class_code.sub as u32)
+                {
+                    if let HeaderType::Normal(header) = header.header_type.clone() {
+                        let bar0 = header.base_addresses.orig()[0];
+                        let bar1 = header.base_addresses.orig()[1];
 
-            let regs = unsafe { Registers::new(full_bar as usize, MAPPER.write().clone()) };
+                        let full_bar = bar0 as u64 | ((bar1 as u64) << 32);
 
-            Some(regs)
-        } else {
-            None
+                        let regs = unsafe { Registers::new(full_bar as usize, MAPPER.write().clone()) };
+
+                        Some(regs)
+                    } else {
+                        None
+                    }
+                } else {
+                    None
+                }
+            }
         }
-    } else {
-        None
+    }
+}
+
+impl FOSSPciDeviceHandle for XhciImpl {
+    fn handles(&self, vendor_id: crate::pci_impl::Vendor, device_id: DeviceKind) -> bool {
+        matches!(
+            (vendor_id, device_id),
+            (crate::pci_impl::Vendor::Intel, DeviceKind::UsbController)
+        )
+    }
+
+    fn start(&self, header: &mut pcics::Header) {
+        todo!("actually implement {:#?} here", header);
     }
 }
