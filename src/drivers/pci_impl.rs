9bee4eb61fbe2f3bf96a093253ecb66de91a1519
Kenny Strawn <Kenny.Strawn@gmail.com>
2023-10-07 11:46:21 -0700

    phil-opp/blog_os#1248

--- src/drivers/pci_impl.rs
+++ src/drivers/pci_impl.rs
@@ -921,6 +921,8 @@ pub fn init(tables: &AcpiTables<KernelAcpi>) {
             });
 
             if let Some(msix) = msix {
+                // Most of this was learned from studying Aero's implementation:
+                // https://github.com/Andy-Python-Programmer/aero/blob/master/src/aero_kernel/src/drivers/pci.rs#L99
                 if let CapabilityKind::MsiX(mut msix) = msix.kind {
                     // borrow checker
                     let msix_clone = msix.clone();
