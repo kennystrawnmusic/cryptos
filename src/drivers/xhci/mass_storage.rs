d33cddac9baf3a231d3ef6541144a3ecbd48a6ae
Kenny Strawn <Kenny.Strawn@gmail.com>
2023-10-12 14:21:50 -0700

    Begin basic mass storage structure creation

--- src/drivers/xhci/mass_storage.rs
+++ src/drivers/xhci/mass_storage.rs
@@ -1,1 +1,16 @@
 use super::*;
+
+use alloc::sync::Arc;
+use spin::RwLock;
+
+#[allow(dead_code)]  // not ready to use this yet
+pub struct MassStorageDriver<const N: usize> {
+    xhci: Arc<RwLock<XhciImpl>>,
+    device: Arc<RwLock<Device<N>>>,
+    slot_id: u8,
+    endpoint: u8,
+    max_packet_size: u16,
+    max_lun: u8,
+    block_size: u32,
+    block_count: u32,
+}
