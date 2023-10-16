7aedbd6177960aeab41359d6496237faca720c26
Kenny Strawn <Kenny.Strawn@gmail.com>
2023-10-16 13:43:11 -0700

    Mass storage progress

--- src/drivers/xhci/mass_storage.rs
+++ src/drivers/xhci/mass_storage.rs
@@ -1,16 +1,88 @@
+use core::marker::PhantomData;
+
 use super::*;
 
-use alloc::sync::Arc;
+use alloc::{sync::Arc, boxed::Box};
 use spin::RwLock;
+use xhci::context::{DeviceHandler, EndpointHandler, SlotHandler};
+
+pub enum DeviceKind {
+    Device8(Device<8>),
+    Device16(Device<16>),
+}
+
+impl DeviceKind {
+    pub fn slot<'a>(&'a self) -> &'a dyn SlotHandler {
+        match self {
+            Self::Device8(device) => device.slot(),
+            Self::Device16(device) => device.slot(),
+        }
+    }
+
+    pub fn slot_mut<'a>(&'a mut self) -> &'a mut dyn SlotHandler {
+        match self {
+            Self::Device8(device) => device.slot_mut(),
+            Self::Device16(device) => device.slot_mut(),
+        }
+    }
+
+    pub fn endpoint<'a>(&'a self, index: usize) -> &'a dyn EndpointHandler {
+        match self {
+            Self::Device8(device) => device.endpoint(index),
+            Self::Device16(device) => device.endpoint(index),
+        }
+    }
+
+    pub fn endpoint_mut<'a>(&'a mut self, index: usize) -> &'a mut dyn EndpointHandler {
+        match self {
+            Self::Device8(device) => device.endpoint_mut(index),
+            Self::Device16(device) => device.endpoint_mut(index),
+        }
+    }
+}
+
+impl From<usize> for DeviceKind {
+    fn from(size: usize) -> Self {
+        match size {
+            8 => Self::Device8(Device::new_32byte()),
+            16 => Self::Device16(Device::new_64byte()),
+            _ => panic!("Unsupported device size"),
+        }
+    }
+}
 
 #[allow(dead_code)]  // not ready to use this yet
-pub struct MassStorageDriver<const N: usize> {
-    xhci: Arc<RwLock<XhciImpl>>,
-    device: Arc<RwLock<Device<N>>>,
+pub struct MassStorageDriver<'a, const N: usize> {
+    xhci: Arc<RwLock<&'a mut XhciImpl>>,
+    device: Arc<RwLock<DeviceKind>>,
     slot_id: u8,
-    endpoint: u8,
     max_packet_size: u16,
-    max_lun: u8,
-    block_size: u32,
-    block_count: u32,
+    block_size: usize,
+    block_count: usize,
+}
+
+impl<'a, const N: usize> MassStorageDriver<'a, N> {
+    pub fn new(xhci: &'a mut XhciImpl, endpoint: usize, transfer_block: usize, transfer_len: usize) -> Self {
+        let mut device = DeviceKind::from(N);
+
+        let slot_id = device.slot().usb_device_address();
+
+        // borrow checker
+        let slot_id = slot_id.clone();
+
+        let endpoint = device.endpoint_mut(endpoint);
+
+        let max_packet_size = endpoint.max_packet_size();
+        let block_size = transfer_block;
+        let block_count = transfer_len;
+
+        Self {
+            xhci: Arc::new(RwLock::new(xhci)),
+            device: Arc::new(RwLock::new(DeviceKind::from(N))),
+            slot_id: slot_id.clone(),
+            max_packet_size,
+            block_size,
+            block_count,
+        }
+    }
 }
