eedb6e91ab67280fd9984d93bba8182b837942c1
Kenny Strawn <Kenny.Strawn@gmail.com>
2023-10-21 21:58:46 -0700

    Fix comments

--- src/drm/mod.rs
+++ src/drm/mod.rs
@@ -80,7 +80,8 @@ impl PixelColorKind {
         }
     }
 
-    // Safety: Attempting to call this before AVX is initialized or after it's disabled again will cause a #GP
+    // Safety: Attempting to call this before AVX is initialized or after it's disabled again
+    // will cause a general protection fault
     unsafe fn alpha_blend_inner(&self, alpha: f32, other: PixelColorKind) -> Self {
         if !(0.0..=1.0).contains(&alpha) {
             panic!("Alpha value must be a value between 0 and 1");
