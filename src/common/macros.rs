e0718f6817162348d497c701aed5e1bcaf72b8a9
Kenny Strawn <kstrawn0@saddleback.edu>
2024-02-12 17:05:06 -0800

    `map_range_inclusive\!`: Add more options

--- src/common/macros.rs
+++ src/common/macros.rs
@@ -211,29 +211,21 @@ macro_rules! printk {
 // Needed for system call interface
 #[macro_export]
 macro_rules! map_range_inclusive {
-    ($start:expr, $end:expr, $size:ty, $flags:expr) => {
-        let start = $start;
-        let end = $end;
-
-        let phys_frame_range = $crate::cralloc::frames::FRAME_ALLOCATOR
-            .allocate_multiple((end - start) as usize / 4096)
-            .expect("Out of memory")
-            .0;
-
+    ($phys_start:expr, $phys_end:expr, $virt_start:expr, $virt_end:expr, $size:expr, $flags:expr) => {
+        let phys_frame_range = {
+            let start = x86_64::structures::paging::PhysFrame::containing_address(x86_64::PhysAddr::new($phys_start as u64));
+            let end = x86_64::structures::paging::PhysFrame::containing_address(x86_64::PhysAddr::new($phys_end as u64));
+            x86_64::structures::paging::PhysFrame::range_inclusive(start, end)
+        };
+        
         let page_range = {
-            let first_page = Page::<$size>::containing_address(VirtAddr::new(start as u64));
-            let last_page = Page::<$size>::containing_address(VirtAddr::new(end as u64));
-
-            Page::<$size>::range_inclusive(first_page, last_page)
+            let start = x86_64::structures::paging::Page::<$size>::containing_address(x86_64::VirtAddr::new($virt_start as u64));
+            let end = x86_64::structures::paging::Page::<$size>::containing_address(x86_64::VirtAddr::new($virt_end as u64));
+            x86_64::structures::paging::Page::<$size>::range_inclusive(start, end)
         };
 
         for (p, f) in page_range.zip(phys_frame_range) {
-            map_page!(
-                f.start_address().as_u64(),
-                p.start_address().as_u64(),
-                $size,
-                $flags
-            );
+            map_page!(f.start_address().as_u64(), p.start_address().as_u64(), $size, $flags);
         }
     };
 }
