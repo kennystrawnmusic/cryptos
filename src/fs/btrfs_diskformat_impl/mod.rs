3de0239c9fd5024eee812a610e28777edc8d8b8c
Kenny Strawn <Kenny.Strawn@gmail.com>
2024-05-07 07:50:08 -0700

    btrfs_diskformat: Progress

--- src/fs/btrfs_diskformat_impl/mod.rs
+++ src/fs/btrfs_diskformat_impl/mod.rs
@@ -1,6 +1,6 @@
 use core::sync::atomic::{AtomicU64, Ordering};
 
-use crate::{ahci::AhciPort, drivers::ahci::get_ahci};
+use crate::drivers::ahci::get_ahci;
 use alloc::boxed::Box;
 use btrfs_diskformat::{constants::PRIMARY_SUPERBLOCK_ADDR, *};
 use zerocopy::{U32, U64};
@@ -28,48 +28,31 @@ impl BtrfsDriver {
     }
     // code won't be dead for long
     #[allow(dead_code)]
-    pub fn find_superblock_on_ahci_disk(
-        &self,
-        _controller: AhciPort,
-        disk: usize,
-    ) -> Result<DevItem, &'static str> {
+    pub fn find_superblock_on_ahci_disk(&self, disk: usize) -> Result<DevItem, &'static str> {
         // Use already-existing AHCI driver to find superblock location at offset 0x10000
         let buffer = unsafe { core::slice::from_raw_parts_mut(Box::into_raw(Box::new(0u8)), 4096) };
 
-        if let Some(_) = get_ahci()
-            .write()
-            .port_mut(disk)?
-            .read(PRIMARY_SUPERBLOCK_ADDR as usize, buffer)
-        {
-            Ok(DevItem {
-                devid: U64::new(self.dev_id.fetch_add(1, Ordering::SeqCst)),
-                total_bytes: U64::new(
-                    get_ahci()
-                        .write()
-                        .port_mut(disk)?
-                        .identify()
-                        .unwrap_or_else(|| 0u64),
-                ),
-                bytes_used: U64::new(0), // TODO: use AHCI, XHCI, and planned NVMe drivers to detect this
-                io_align: U32::new(0), // TODO: use AHCI, XHCI, and planned NVMe drivers to detect this
-                io_width: U32::new(0), // TODO: use AHCI, XHCI, and planned NVMe drivers to detect this
-                sector_size: U32::new(
-                    (get_ahci()
-                        .write()
-                        .port_mut(disk)?
-                        .identify()
-                        .unwrap_or_else(|| 0u64)
-                        % 512) as u32,
-                ),
-                r#type: U64::new(0), // TODO: use AHCI, XHCI, and planned NVMe drivers to detect this
-                generation: U64::new(0), // TODO: use AHCI, XHCI, and planned NVMe drivers to detect this
-                start_offset: U64::new(0), // TODO: use AHCI, XHCI, and planned NVMe drivers to detect this
-                dev_group: U32::new(0), // TODO: use AHCI, XHCI, and planned NVMe drivers to detect this
-                seek_speed: 0, // TODO: use AHCI, XHCI, and planned NVMe drivers to detect this
-                bandwith: 0,   // TODO: use AHCI, XHCI, and planned NVMe drivers to detect this
-                uuid: [0; 16], // TODO: use AHCI, XHCI, and planned NVMe drivers to detect this
-                fsid: [0; 16], // TODO: use AHCI, XHCI, and planned NVMe drivers to detect this
-            })
+        if let Ok(port) = get_ahci().write().port_mut(disk) {
+            if let Some(_) = port.read(PRIMARY_SUPERBLOCK_ADDR as usize, buffer) {
+                Ok(DevItem {
+                    devid: U64::new(self.dev_id.fetch_add(1, Ordering::SeqCst)),
+                    total_bytes: U64::new(port.identify().unwrap_or_else(|| 0u64)),
+                    bytes_used: U64::new(0), // TODO: use AHCI, XHCI, and planned NVMe drivers to detect this
+                    io_align: U32::new(0), // TODO: use AHCI, XHCI, and planned NVMe drivers to detect this
+                    io_width: U32::new(0), // TODO: use AHCI, XHCI, and planned NVMe drivers to detect this
+                    sector_size: U32::new((port.identify().unwrap_or_else(|| 0u64) % 512) as u32),
+                    r#type: U64::new(0), // TODO: use AHCI, XHCI, and planned NVMe drivers to detect this
+                    generation: U64::new(0), // TODO: use AHCI, XHCI, and planned NVMe drivers to detect this
+                    start_offset: U64::new(0), // TODO: use AHCI, XHCI, and planned NVMe drivers to detect this
+                    dev_group: U32::new(0), // TODO: use AHCI, XHCI, and planned NVMe drivers to detect this
+                    seek_speed: 0, // TODO: use AHCI, XHCI, and planned NVMe drivers to detect this
+                    bandwith: 0,   // TODO: use AHCI, XHCI, and planned NVMe drivers to detect this
+                    uuid: [0; 16], // TODO: use AHCI, XHCI, and planned NVMe drivers to detect this
+                    fsid: [0; 16], // TODO: use AHCI, XHCI, and planned NVMe drivers to detect this
+                })
+            } else {
+                Err("Failed to read superblock")
+            }
         } else {
             Err("Failed to read superblock")
         }
